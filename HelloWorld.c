43cb221b8cefdd18803bb45d20c9fdd03b6a7e62
Abdallahghanemm <abdallahghanem717@gmail.com>
2021-10-15 10:01:19 +0200

    2nd upload

---
+++ HelloWorld.c
@@ -0,0 +1,16 @@
+// single line comment
+// standard input output
+
+#include <stdio.h>
+
+/* this is the main funtction
+printf for pritning */
+
+int main(void){
+	
+	printf("\ng\n\th\n\t\ta\n\t\t\tn\n\t\t\t\tm"); 
+	printf("\n\"ghanem\"");
+	
+  return 0;
+	
+}
---
+++ task.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main (void){
+	printf("\t*");
+	printf("\n       ***");
+	printf("\n      **G**" );
+	printf("\n     *******");
+	printf("\n    *********\n");
+
+	printf("\t*\n       ***\n      **H**\n     *******\n    *********\n");
+	
+	printf("\t*\n       ***\n      **A**\n     *******\n    *********\n");
+	
+	printf("\t*\n       ***\n      **N**\n     *******\n    *********\n");
+	
+	printf("\t*\n       ***\n      **E**\n     *******\n    *********\n");
+	
+	printf("\t*\n       ***\n      **M**\n     *******\n    *********");
+	
+}
